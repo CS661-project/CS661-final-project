d8565a8979392650849052a46d327d88cfaac4a2
aastikg20 <aastikg20@iitk.ac.in>
2024-04-12 11:44:43 +0530

    Added sidebar toggle button

--- dash/app.py
+++ dash/app.py
@@ -1,9 +1,57 @@
 import dash
-from dash import Dash, html, dcc, Output, Input
+from dash import Dash, html, dcc, Output, Input, State
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.BOOTSTRAP])
 parity=0
+SIDEBAR_OPEN = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "150px",
+    "background-color": "black",
+    "transition": "all 0.5s",
+    "padding": "0.5rem 1rem",
+    "color": "white",
+    "padding-top": "50px",
+}
+
+SIDEBAR_CLOSE = {
+    "position": "fixed",
+    "top": 0,
+    "left": "-150PX",
+    "bottom": 0,
+    "width": "150px",
+    "background-color": "black",
+    "transition": "all 0.5s",
+    "padding": "0rem 0rem",
+    "color": "white",
+    "padding-top": "50px",
+}
+
+CONTENT_STYLE = {
+    "transition": "margin-left .5s",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem"
+}
+
+CONTENT_STYLE1 = {
+    "transition": "margin-left .5s",
+    "margin-left": "-150px",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem"
+}
+
+SIDEBAR_TOGGLE_STYLE_CLOSE = {
+    "transition": "margin-left .5s",
+    "margin-left": "-150px",
+}
+
+SIDEBAR_TOGGLE_STYLE_OPEN = {
+    "transition": "margin-left .5s",
+    "margin-left": "0px",
+}
 # Define the sidebar content
 sidebar = html.Div([
     html.Div([
@@ -16,17 +64,53 @@ sidebar = html.Div([
         html.Button('Comparison page', id='pg7', n_clicks=0, className="button_style"),
         html.Button('Settings', id='settings', n_clicks=0, className="button_style"),
     ])
-], className="sidebar_style")
+], style=SIDEBAR_OPEN, id="sidebar")
 
 # Define the initial content as an empty div
 initial_content = html.Div([
+    dcc.Store(id='side_click'),
     dcc.Location(id='url', refresh=False),
-    html.Div(id='initial-content'),
+    dbc.Button("Close Sidebar", color="primary", className="me-1", id="sidebar-toggle", style=SIDEBAR_TOGGLE_STYLE_OPEN),
+    html.Div(id='initial-content',style=CONTENT_STYLE),
 ], className="content_style")
 
 
 # Define the app layout
 app.layout = html.Div([sidebar, initial_content])
+
+@app.callback(
+        [Output("sidebar", "style"),
+        Output("initial-content", "style"),
+        Output("side_click", "data"),
+        Output("sidebar-toggle", "style")],
+        Output("sidebar-toggle","children"),
+        Input("sidebar-toggle", "n_clicks"),
+        [
+        State("side_click", "data"),
+    ]
+)
+def toggle_sidebar(n, nclick):
+    if n:
+        if nclick == "SHOW":
+            sidebar_style = SIDEBAR_CLOSE
+            content_style = CONTENT_STYLE1
+            sidebar_toggle_style = SIDEBAR_TOGGLE_STYLE_CLOSE
+            sidebar_toggle_value = "Open Sidebar"
+            cur_nclick = "HIDDEN"
+        else:
+            sidebar_style = SIDEBAR_OPEN
+            content_style = CONTENT_STYLE
+            sidebar_toggle_style = SIDEBAR_TOGGLE_STYLE_OPEN
+            sidebar_toggle_value = "Close Sidebar"
+            cur_nclick = "SHOW"
+    else:
+        sidebar_style = SIDEBAR_OPEN
+        content_style = CONTENT_STYLE
+        sidebar_toggle_style = SIDEBAR_TOGGLE_STYLE_OPEN
+        sidebar_toggle_value = "Close Sidebar"
+        cur_nclick = 'SHOW'
+
+    return sidebar_style, content_style, cur_nclick, sidebar_toggle_style, sidebar_toggle_value
 
 # Callback to update the initial content when "First" button is clicked
 @app.callback(
