59a1e25663f5db9dc1c47845abe819c1fb2aedf7
overeater <manankalavadia@gmail.com>
2024-04-12 17:38:42 +0530

    Rough Animations added

--- dash/pages/page4.py
+++ dash/pages/page4.py
@@ -12,6 +12,8 @@ import time
 
 dash.register_page(__name__,order=3)
 
+
+
 count_list= ['No', 'Aruba', 'Afghanistan', 'Angola', 'Albania', 'Andorra', 'United Arab Emirates', 'Argentina', 'Armenia', 'American Samoa', 'Antigua and Barbuda', 'Australia', 'Austria', 'Azerbaijan', 'Burundi', 'Belgium', 'Benin', 'Burkina Faso', 'Bangladesh', 'Bulgaria', 'Bahrain', 'Bahamas, The', 'Bosnia and Herzegovina', 'Belarus', 'Belize', 'Bermuda', 'Bolivia', 'Brazil', 'Barbados', 'Brunei Darussalam', 'Bhutan', 'Botswana', 'Central African Republic', 'Canada', 'Switzerland', 'Channel Islands', 'Chile', 'China', "Cote d'Ivoire", 'Cameroon', 'Congo, Dem. Rep.', 'Congo, Rep.', 'Colombia', 'Comoros', 'Cabo Verde', 'Costa Rica', 'Cuba', 'Curacao', 'Cayman Islands', 'Cyprus', 'Czechia', 'Germany', 'Djibouti', 'Dominica', 'Denmark', 'Dominican Republic', 'Algeria', 'Ecuador', 'Egypt, Arab Rep.', 'Eritrea', 'Spain', 'Estonia', 'Ethiopia', 'Finland', 'Fiji', 'France', 'Faroe Islands', 'Micronesia, Fed. Sts.', 'Gabon', 'United Kingdom', 'Georgia', 'Ghana', 'Gibraltar', 'Guinea', 'Gambia, The', 'Guinea-Bissau', 'Equatorial Guinea', 'Greece', 'Grenada', 'Greenland', 'Guatemala', 'Guam', 'Guyana', 'Hong Kong SAR, China', 'Honduras', 'Croatia', 'Haiti', 'Hungary', 'Indonesia', 'Isle of Man', 'India', 'Ireland', 'Iran, Islamic Rep.', 'Iraq', 'Iceland', 'Israel', 'Italy', 'Jamaica', 'Jordan', 'Japan', 'Kazakhstan', 'Kenya', 'Kyrgyz Republic', 'Cambodia', 'Kiribati', 'St. Kitts and Nevis', 'Korea, Rep.', 'Kuwait', 'Lao PDR', 'Lebanon', 'Liberia', 'Libya', 'St. Lucia', 'Liechtenstein', 'Sri Lanka', 'Lesotho', 'Lithuania', 'Luxembourg', 'Latvia', 'Macao SAR, China', 'St. Martin (French part)', 'Morocco', 'Monaco', 'Moldova', 'Madagascar', 'Maldives', 'Mexico', 'Marshall Islands', 'North Macedonia', 'Mali', 'Malta', 'Myanmar', 'Montenegro', 'Mongolia', 'Northern Mariana Islands', 'Mozambique', 'Mauritania', 'Mauritius', 'Malawi', 'Malaysia', 'Namibia', 'New Caledonia', 'Niger', 'Nigeria', 'Nicaragua', 'Netherlands', 'Norway', 'Nepal', 'Nauru', 'New Zealand', 'Oman', 'Pakistan', 'Panama', 'Peru', 'Philippines', 'Palau', 'Papua New Guinea', 'Poland', 'Puerto Rico', "Korea, Dem. People's Rep.", 'Portugal', 'Paraguay', 'West Bank and Gaza', 'French Polynesia', 'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'Saudi Arabia', 'Sudan', 'Senegal', 'Singapore', 'Solomon Islands', 'Sierra Leone', 'El Salvador', 'San Marino', 'Somalia', 'Serbia', 'South Sudan', 'Sao Tome and Principe', 'Suriname', 'Slovak Republic', 'Slovenia', 'Sweden', 'Eswatini', 'Sint Maarten (Dutch part)', 'Seychelles', 'Syrian Arab Republic', 'Turks and Caicos Islands', 'Chad', 'Togo', 'Thailand', 'Tajikistan', 'Turkmenistan', 'Timor-Leste', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkiye', 'Tuvalu', 'Tanzania', 'Uganda', 'Ukraine', 'Uruguay', 'United States', 'Uzbekistan', 'St. Vincent and the Grenadines', 'Venezuela, RB', 'British Virgin Islands', 'Virgin Islands (U.S.)', 'Viet Nam', 'Vanuatu', 'Samoa', 'Kosovo', 'Yemen, Rep.', 'South Africa', 'Zambia', 'Zimbabwe'] 
 yr_list=['1960','1961','1962','1963','1964','1965','1966','1967','1968','1969','1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022']
                 
@@ -26,6 +28,7 @@ mask = df_gdp['Country Name'] == 'World'
 df_gdp=df_gdp[~mask]
 df_gdp.set_index("Country Name", inplace = True)
 
+year_manan = "2022"
 
 def generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4):
     # time.sleep(2)
@@ -58,16 +61,7 @@ def generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4):
                 name=country
             ))
     else:
-        max_gdp=1
-        g=0
-        p=0
-        s=0
-        t=0
-        fig = go.Figure(data=go.Scatterpolar(
-            r=[p,s,t,(g/max_gdp)*100],
-            theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP'],
-            fill='toself'
-        ))
+        fig = generate_empty_chart()
 
     fig.update_layout(
         polar=dict(
@@ -78,17 +72,20 @@ def generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4):
         ),
         showlegend=False
     )
-
+    
     return fig
 
 def generate_sectoral_animation(country_dropdown_pg4,year_dropdown_pg4):
     # time.sleep(2)
+    if(year_dropdown_pg4==None):
+        return generate_empty_chart()
     if(len(country_dropdown_pg4)==0):
         return generate_empty_chart()
     if(type(country_dropdown_pg4)==list):
         country_dropdown_pg4 = country_dropdown_pg4[0]
-    max_gdp=df_gdp[year_dropdown_pg4].max()
-    g=df_gdp.loc[country_dropdown_pg4, year_dropdown_pg4]
+    gdp_series = df_gdp.loc[country_dropdown_pg4][3:-1]  # Exclude the first two columns (Country Code and Indicator Name) and the last column (2023)
+    max_gdp = gdp_series.max()
+    g=df_gdp.loc[country_dropdown_pg4,year_dropdown_pg4]
     p=df_primary.loc[country_dropdown_pg4, year_dropdown_pg4]
     s=df_secondary.loc[country_dropdown_pg4, year_dropdown_pg4]
     t=df_tertiary.loc[country_dropdown_pg4, year_dropdown_pg4]
@@ -106,7 +103,6 @@ def generate_sectoral_animation(country_dropdown_pg4,year_dropdown_pg4):
         ),
         showlegend=False
     )
-
     return fig
 
 @callback(
@@ -120,11 +116,26 @@ def update_layout(mode_dropdown_pg4,country_dropdown_pg4,year_dropdown_pg4):
     if mode_dropdown_pg4 == 'Countries':
         return generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4)
     elif mode_dropdown_pg4 == 'Animation':                                                                       #animation left ------> not able to do it, may add slider instead
-        for i in yr_list:
-            if int(i)>2015:
-                return generate_sectoral_animation(country_dropdown_pg4,str(i))
+        print("Animation")
+        return generate_sectoral_animation(country_dropdown_pg4,year_dropdown_pg4)
     else:
         return generate_empty_chart()
+
+
+@callback(
+        Output("year_dropdown_pg4", "value"),
+        Input("year_dropdown_pg4", "value"),
+        Input("interval-component", "n_intervals"),
+        Input("mode_dropdown_pg4", "value"),
+        Prevent_initial_update = True,
+)
+def recall(year_dropdow_pg4, n_intervals, mode_dropdown_pg4):
+    print(year_dropdow_pg4)
+    if mode_dropdown_pg4=='Animation':
+        if(int(year_dropdow_pg4)>2022):
+            return "1991"
+        return str(int(year_dropdow_pg4)+1)
+    return year_dropdow_pg4
 
 def generate_empty_chart():
     max_gdp=1
@@ -156,6 +167,7 @@ layout = html.Div([
             options=['Countries', 'Animation'],
             value="Countries",
             id="mode_dropdown_pg4",
+            # persistence=True,
             style={"width": "40%"}
         ),
     dcc.Dropdown(
@@ -169,7 +181,13 @@ layout = html.Div([
                 options=yr_list,
                 value="2018",
                 id="year_dropdown_pg4",
+                # persistence = True,
                 style={"width": "40%"}
             ),
-    dcc.Graph(id="sectoral_chart")
+    dcc.Graph(id="sectoral_chart"),
+    dcc.Interval(
+        id='interval-component',
+        interval=1*1000,  # in milliseconds
+        n_intervals=0
+    ),
 ])
