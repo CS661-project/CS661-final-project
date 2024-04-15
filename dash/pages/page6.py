a0ba8abceca552d7f8a6fe86614a11d0cafc47c0
overeater <manankalavadia@gmail.com>
2024-04-15 15:15:27 +0530

    Callback error and Year dropdown box hidden

--- dash/pages/page6.py
+++ dash/pages/page6.py
@@ -414,6 +414,8 @@ def change_options(year_dropdown_pg1):
     return settings.global_options
 
 def generate_animation_bar(factor_pg6):
+    if(factor_pg6==None):
+        return px.bar()
     df_fact=pd.read_csv('./Data_files/'+factor_pg6+'.csv')
     val=["Upper middle income","Lower middle income","Low income","High income"]
     df_fact=df_fact[df_fact['Country Name'].isin(val)]
@@ -454,7 +456,8 @@ layout = html.Div([
                 options=['1960','1961','1962','1963','1964','1965','1966','1967','1968','1969','1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022'],
                 value="2018",
                 id="year_dropdown_pg6",
-                style={"width": "100%"}
+                # style={"width": "100%"},
+                style={"display": "None"}
             ),
     dcc.Dropdown(
                 options=[
