2a9f6036578fe3e4ac50b804e58b35a20fbb7d7d
overeater <manankalavadia@gmail.com>
2024-04-15 14:35:55 +0530

    Legend added, size done. Hopefully completed

--- dash/pages/page4.py
+++ dash/pages/page4.py
@@ -15,7 +15,7 @@ dash.register_page(__name__,order=5)
 external_stylesheets = ['https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css', '/assets/page4.css']
 
 count_list= ['No', 'Aruba', 'Afghanistan', 'Angola', 'Albania', 'Andorra', 'United Arab Emirates', 'Argentina', 'Armenia', 'American Samoa', 'Antigua and Barbuda', 'Australia', 'Austria', 'Azerbaijan', 'Burundi', 'Belgium', 'Benin', 'Burkina Faso', 'Bangladesh', 'Bulgaria', 'Bahrain', 'Bahamas, The', 'Bosnia and Herzegovina', 'Belarus', 'Belize', 'Bermuda', 'Bolivia', 'Brazil', 'Barbados', 'Brunei Darussalam', 'Bhutan', 'Botswana', 'Central African Republic', 'Canada', 'Switzerland', 'Channel Islands', 'Chile', 'China', "Cote d'Ivoire", 'Cameroon', 'Congo, Dem. Rep.', 'Congo, Rep.', 'Colombia', 'Comoros', 'Cabo Verde', 'Costa Rica', 'Cuba', 'Curacao', 'Cayman Islands', 'Cyprus', 'Czechia', 'Germany', 'Djibouti', 'Dominica', 'Denmark', 'Dominican Republic', 'Algeria', 'Ecuador', 'Egypt, Arab Rep.', 'Eritrea', 'Spain', 'Estonia', 'Ethiopia', 'Finland', 'Fiji', 'France', 'Faroe Islands', 'Micronesia, Fed. Sts.', 'Gabon', 'United Kingdom', 'Georgia', 'Ghana', 'Gibraltar', 'Guinea', 'Gambia, The', 'Guinea-Bissau', 'Equatorial Guinea', 'Greece', 'Grenada', 'Greenland', 'Guatemala', 'Guam', 'Guyana', 'Hong Kong SAR, China', 'Honduras', 'Croatia', 'Haiti', 'Hungary', 'Indonesia', 'Isle of Man', 'India', 'Ireland', 'Iran, Islamic Rep.', 'Iraq', 'Iceland', 'Israel', 'Italy', 'Jamaica', 'Jordan', 'Japan', 'Kazakhstan', 'Kenya', 'Kyrgyz Republic', 'Cambodia', 'Kiribati', 'St. Kitts and Nevis', 'Korea, Rep.', 'Kuwait', 'Lao PDR', 'Lebanon', 'Liberia', 'Libya', 'St. Lucia', 'Liechtenstein', 'Sri Lanka', 'Lesotho', 'Lithuania', 'Luxembourg', 'Latvia', 'Macao SAR, China', 'St. Martin (French part)', 'Morocco', 'Monaco', 'Moldova', 'Madagascar', 'Maldives', 'Mexico', 'Marshall Islands', 'North Macedonia', 'Mali', 'Malta', 'Myanmar', 'Montenegro', 'Mongolia', 'Northern Mariana Islands', 'Mozambique', 'Mauritania', 'Mauritius', 'Malawi', 'Malaysia', 'Namibia', 'New Caledonia', 'Niger', 'Nigeria', 'Nicaragua', 'Netherlands', 'Norway', 'Nepal', 'Nauru', 'New Zealand', 'Oman', 'Pakistan', 'Panama', 'Peru', 'Philippines', 'Palau', 'Papua New Guinea', 'Poland', 'Puerto Rico', "Korea, Dem. People's Rep.", 'Portugal', 'Paraguay', 'West Bank and Gaza', 'French Polynesia', 'Qatar', 'Romania', 'Russian Federation', 'Rwanda', 'Saudi Arabia', 'Sudan', 'Senegal', 'Singapore', 'Solomon Islands', 'Sierra Leone', 'El Salvador', 'San Marino', 'Somalia', 'Serbia', 'South Sudan', 'Sao Tome and Principe', 'Suriname', 'Slovak Republic', 'Slovenia', 'Sweden', 'Eswatini', 'Sint Maarten (Dutch part)', 'Seychelles', 'Syrian Arab Republic', 'Turks and Caicos Islands', 'Chad', 'Togo', 'Thailand', 'Tajikistan', 'Turkmenistan', 'Timor-Leste', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkiye', 'Tuvalu', 'Tanzania', 'Uganda', 'Ukraine', 'Uruguay', 'United States', 'Uzbekistan', 'St. Vincent and the Grenadines', 'Venezuela, RB', 'British Virgin Islands', 'Virgin Islands (U.S.)', 'Viet Nam', 'Vanuatu', 'Samoa', 'Kosovo', 'Yemen, Rep.', 'South Africa', 'Zambia', 'Zimbabwe'] 
-yr_list=['1960','1961','1962','1963','1964','1965','1966','1967','1968','1969','1970','1971','1972','1973','1974','1975','1976','1977','1978','1979','1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022']
+yr_list=['1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022']
                 
 df_gdp = pd.read_csv('./Data_files/gdp_per_capita_updated.csv')
 df_primary = pd.read_csv('./Data_files/percent_pop_in_agri_updated.csv')
@@ -49,7 +49,7 @@ def generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4):
         t=df_tertiary.loc[country_dropdown_pg4, year_dropdown_pg4]
         fig = go.Figure(data=go.Scatterpolar(
             r=[p,s,t,(g/max_gdp)*100],
-            theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP'],
+            theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP per capita'],
             fill='toself'
         ))
     elif len(country_dropdown_pg4)>=1 and type(year_dropdown_pg4) == str:
@@ -63,7 +63,7 @@ def generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4):
             t=df_tertiary.loc[country, year_dropdown_pg4]
             fig.add_trace(go.Scatterpolar(
                 r=[p,s,t,(g/max_gdp)*100],
-                theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP'],
+                theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP per capita'],
                 fill='toself',
                 name=country
             ))
@@ -77,7 +77,7 @@ def generate_sectoral_chart(country_dropdown_pg4,year_dropdown_pg4):
             range=[0, 100]
             ),
         ),
-        showlegend=False
+        showlegend=True
     )
 
     return fig
@@ -100,7 +100,7 @@ def generate_sectoral_animation(country_dropdown_pg4,year_dropdown_pg4):
         t=df_tertiary.loc[country_dropdown_pg4, year_dropdown_pg4]
         fig = go.Figure(data=go.Scatterpolar(
             r=[p,s,t,(g/max_gdp)*100],
-            theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP'],
+            theta=['Primary Sector','Secondary Sector','Tertiary Sector','Normalized GDP per capita'],
             fill='toself'
         ))
         fig.update_layout(
@@ -110,7 +110,7 @@ def generate_sectoral_animation(country_dropdown_pg4,year_dropdown_pg4):
                 range=[0, 100]
                 ),
             ),
-            showlegend=False
+            showlegend=True
         )
     return fig
 
@@ -170,7 +170,7 @@ def generate_empty_chart():
             range=[0, 100]
             ),
         ),
-        showlegend=False
+        showlegend=True
     )
 
     return fig
@@ -193,14 +193,14 @@ layout = html.Div([
                  ),
                  dcc.Dropdown(
                      options=count_list,
-                     value="India",
+                     value=[],
                      id="country_dropdown_pg4",
                      multi=True,
                      style={"width": "40%"}
                  ),
                  dcc.Dropdown(
                      options=yr_list,
-                     value="2018",
+                     value=None,
                      id="year_dropdown_pg4",
                      style={"width": "40%"}
                  ),
@@ -212,7 +212,7 @@ layout = html.Div([
                  dcc.Graph(id="sectoral_chart"),
                  dcc.Interval(
                      id='interval-component',
-                     interval=1 * 1500,  # in milliseconds
+                     interval=1 * 700,  # in milliseconds
                      n_intervals=0
                  ),
              ]
